e9bcf0475a452a8e8a21aebb827bf15bbb46d929
James Williamson <jwilliamson@gmail.com>
2010-03-02 16:34:37 +0000

    Made rake script more advanced, can now do find and replace on a file

--- Trunk/Ruby/Rake/rakefile.rb
+++ Trunk/Ruby/Rake/rakefile.rb
@@ -7,12 +7,11 @@ SOURCE_DIR = File.expand_path('RakeTest')
 SOLUTION = File.join(SOURCE_DIR, "RakeTest.sln")
 OUTPUTDIRECTORY = File.join(SOURCE_DIR, "RakeTest.Domain/bin/Debug/")
 TESTDLL = File.join(SOURCE_DIR, "RakeTest.UnitTests/bin/Debug/RakeTest.UnitTests.dll")
-PACKAGEDIRECTORY = "RakeTest/Package"
-CONFIG = "Debug"
+PACKAGEDIRECTORY = File.join(SOURCE_DIR, "/Package/")
 
 task :default => [:buildsolution]
 
-task :buildsolution => [:compile, :tests, :copyfiles]
+task :buildsolution => [:compile, :tests, :copyfiles, :replace_values]
 
 msbuild :compile do |msb|
 	msb.properties :configuration => :Debug
@@ -27,14 +26,20 @@ end
 
 
 task :copyfiles do
+	rm_r PACKAGEDIRECTORY
 	mkpath PACKAGEDIRECTORY
-	cp_r OUTPUTDIRECTORY, PACKAGEDIRECTORY
+	cp_r "#{OUTPUTDIRECTORY}.", PACKAGEDIRECTORY
+	
+	@app_config_file_name = File.join(PACKAGEDIRECTORY, "App.config")
+	File.delete(@app_config_file_name)
+	File.rename(File.join(PACKAGEDIRECTORY, "Master_App.config"), @app_config_file_name)
 end
 
-task :create_directories do
-	two_directories = ['folder1', 'folder2']
-	two_directories.each do |directory|
-		puts directory
+task :replace_values do
+	File.open(@app_config_file_name, "r+") do |f|
+		s = f.read.sub(/%%ReplaceMe%%/, "WithMe")
+          f.rewind
+          f.write s
 	end
 end
 
