08939561aecbaadf85d794d78ecd03b872f76582
unknown <Jamie.Williamson@.uswitch.com>
2010-05-12 10:43:25 +0100

    My site speed brown bag

--- uSwitch/uSwitch.SiteSpeed/uSwitch.SiteSpeed.Web/Core/Modules/CacheModule.cs
+++ uSwitch/uSwitch.SiteSpeed/uSwitch.SiteSpeed.Web/Core/Modules/CacheModule.cs
@@ -20,22 +20,26 @@ namespace uSwitch.SiteSpeed.Web.Core.Modules
 
 			string extensions = Path.GetExtension(context.Request.Path);
 
-			if (extensions.ToLowerInvariant().Contains("css") || extensions.ToLowerInvariant().Contains("js"))
-			{
-				TimeSpan cacheDuration = TimeSpan.FromDays(2);
-				var expireTime = DateTime.Now.Add(cacheDuration);
+            if (extensions.ToLowerInvariant().Contains("css")
+                || extensions.ToLowerInvariant().Contains("js")
+                || extensions.ToLowerInvariant().Contains("jpg")
+                || extensions.ToLowerInvariant().Contains("gif"))
+            {
+                TimeSpan cacheDuration = TimeSpan.FromDays(2);
+                var expireTime = DateTime.Now.Add(cacheDuration);
 
-				var response = context.Response;
-				response.Cache.SetExpires(expireTime);
-				response.Cache.SetCacheability(HttpCacheability.Private);
-				response.Cache.SetMaxAge(cacheDuration);
-				response.Cache.AppendCacheExtension("must-revalidate, proxy-revalidate");
-			}
+                var response = context.Response;
+                response.Cache.SetExpires(expireTime);
+                response.Cache.SetCacheability(HttpCacheability.Private);
+                response.Cache.SetNoServerCaching();
+                response.Cache.SetMaxAge(cacheDuration);
+                response.Cache.AppendCacheExtension("must-revalidate, proxy-revalidate");
+            }
 		}
 
 		public void Dispose()
 		{
-			throw new NotImplementedException();
+			
 		}
 	}
 }
--- uSwitch/uSwitch.SiteSpeed/uSwitch.SiteSpeed.Web/Views/Home/Index.aspx
+++ uSwitch/uSwitch.SiteSpeed/uSwitch.SiteSpeed.Web/Views/Home/Index.aspx
@@ -9,4 +9,16 @@
     <p>
         To learn more about ASP.NET MVC visit <a href="http://asp.net/mvc" title="ASP.NET MVC Website">http://asp.net/mvc</a>.
     </p>
+    
+    <ul>
+        <li>
+            <a href="/resource">Resouces page</a>
+        </li>
+        <li>
+            <a href="/resource/cached/10">Cached page</a>
+        </li>
+        <li>
+            <a href="/less">Less page</a>
+        </li>
+    </ul>
 </asp:Content>
